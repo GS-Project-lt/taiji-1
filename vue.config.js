8e7f82ca00fcd08e46e4bf9bb9b0c51b3f94280a
吕涛 <cheerlvtao@163.com>
2020-01-22 11:37:42 +0800

    config update

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports =  {
                 ws: true,
                 changeOrigin: true,
                 pathRewrite: {
-                    '^/api': ''  //通过pathRewrite重写地址，将前缀/api转为/
+                    '^/api': '' 
                 }
             }
         }
